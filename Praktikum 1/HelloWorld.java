20e2797e5bdcc228c607694e7b5957827b04b35f
rafasyaa <100133841+rafasyaa@users.noreply.github.com>
2023-02-22 20:13:14 +0700

    Update HelloWorld.java

--- Praktikum 1/HelloWorld.java
+++ Praktikum 1/HelloWorld.java
@@ -1,3 +1,9 @@
+/**
+* File : HelloWorld.java
+* Penulis : Muhammad Rafasya Putra Ristianto
+* Deskripsi :
+* kelas yang berisi program hello world
+**/
 class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
