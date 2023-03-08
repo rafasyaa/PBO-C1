4bfeb8ca36991e708cc6c5508429f456e2aa6558
rafasyaa <100133841+rafasyaa@users.noreply.github.com>
2023-03-08 20:04:07 +0700

    Add files via upload

---
+++ Praktikum 3/org/poligon/Poligon.java
@@ -0,0 +1,14 @@
+/**
+ * File : Poligon.java 08/03/2023
+ * Penulis : Muhammad Rafasya Putra Ristianto
+ * Deskripsi : representasi dasar dari objek poligon 
+*/
+
+package org.poligon;
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
